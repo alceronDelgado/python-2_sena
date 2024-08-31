51121344add80dfabd1630cb5a14f6977ef5ce87
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-08-31 08:52:50 -0500

    agregado correcciones al taller de bestsellers

--- cuaderno#3.1_taller_pandas/main.py
+++ cuaderno#3.1_taller_pandas/main.py
@@ -1,18 +1,16 @@
-##Dataframe
-
 import pandas as pd
 import numpy as np
 
 #1- Aca se crea el dataframe directamente con read_csv
-df_datos = pd.read_csv('src/bestsellers_with_categories.csv')
+#df_datos = pd.read_csv('bestsellers_with_categories.csv')
+
+df_datos = pd.read_csv('src/bestsellerswithcategories.csv')
 
 #2- Muestra primeros 5 registros
 #print(df_datos.head())
 
-
 #2.1 - Muestra datos estadisticos del dataframe
 #print(df_datos.describe())
-
 
 #3- Agregar una nueva columna y crear numeros aleatorios usando numpy
 
@@ -36,8 +34,6 @@ df_datos['Calificación de la critica'] = calificacion_critica
 
     #Muestra el tipo de dato de cada columna
 #print(df_datos.dtypes)
-
-
 
     #4.2 -Métodos del dataframe
 
@@ -85,6 +81,10 @@ df_datos = pd.DataFrame(df_datos,columns=columns)
 # Creamos nueva columna y aplicamos operación
 df_datos['Calificación Promedio']= round((df_datos['Calificación de la critica'] + df_datos['User Rating'])/2)
 
+#6 operaciones
+# Corregido = HAY QUE COLOCARLE EL 1 PARA INDICARLE QUE ES DE UN SOLO DECIMAL, Si no se coloca, le quita los decimales, se transforma a un ENTERO pero visualmente se ve como un DECIMAL
+df_datos['Calificación Promedio'] = round(df_datos['Calificación de la critica'] + df_datos['User Rating']/2,1)
+
 #Actualizamos el dataframe
 df_datos.update(df_datos)
 #Mostramos en pantalla el dataframe
@@ -106,6 +106,9 @@ df_datos.rename(columns=columnas_nuevas,inplace=True)
 
 #Seleccionar columnas del dataframe
 #print(df_datos[['Name','Author','UR', 'CR', 'AR','Year']])
+
+#Corregido = cambiado la forma de ordenar y actualizar dataframe, solamente es asignar re asignar la variable
+df_datos = df_datos[['Name','Author','UR','CR','AR','Year']]
 
 # 9 ordenar dataframe en orden descendente por UR y CR y actualizar dataframe
 df_datos.sort_values(['UR','CR'],ascending=False,inplace=True)
